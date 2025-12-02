b3cba28809b6f0155b5c6c250731124b3fe2d63b
Jurassickk <cuellardiego310@gmail.com>
2025-12-01 22:14:01 -0500

    docs : refactor article

--- tools/build.sh
+++ tools/build.sh
@@ -1,21 +1,27 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
+set -eu
 
 # Add TeX Live to PATH
 export PATH="/opt/texlive/bin:$PATH"
 
+# Clean previous build
+rm -rf build
+
 mkdir -p build
 
-# Compile IEEE (biblatex + biber)
+# Copy bib file to root for BibTeX
+cp bibliography/references.bib .
+
+# Compile IEEE (BibTeX)
 echo "[build] Compiling IEEE"
-latexmk -silent -file-line-error -outdir=build -xelatex main_ieee.tex
+latexmk -silent -file-line-error -outdir=build -bibtex -pdf main_ieee.tex
 
 # Compile ACM (BibTeX)
 echo "[build] Compiling ACM"
-latexmk -silent -file-line-error -outdir=build -bibtex -xelatex main_acm.tex
+latexmk -silent -file-line-error -outdir=build -bibtex -pdf main_acm.tex
 
-# Compile APA7 (biblatex + biber)
+# Compile APA7 (no bibliography)
 echo "[build] Compiling APA7"
-latexmk -silent -file-line-error -outdir=build -xelatex main_apa7.tex
+latexmk -silent -file-line-error -outdir=build -pdf main_apa7.tex
 
 echo "[build] PDFs available in build/"
